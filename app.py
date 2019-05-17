ac5791f0b1fb042ff0cd7c752111a15373a59e90
MelK44 <50557519+MelK44@users.noreply.github.com>
2019-05-17 16:35:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('display-value', 'figure'),
               [dash.dependencies.Input('dropdown', 'value')])
 def display_value(results):
-    results = df.groupby(user_input)[['calories','fiber','fat','sugars','vitamins','cups']].mean()
+    results = df.groupby(colors_list)[['calories','fiber','fat','sugars','vitamins','cups']].mean()
     mydata = [go.Bar(x = results.index,
                      y = results.values,
                      marker = dict(color='purple'))]
